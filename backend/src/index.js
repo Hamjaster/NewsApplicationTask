f3d29c47fd313b60570e40d44d9ab9ac0cb7bedc
Hamza <hamzashah.dev@gmail.com>
2024-10-10 14:00:27 +0500

    clean code updates

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,41 +1,18 @@
 const express = require("express");
-const axios = require("axios");
-const apicache = require("apicache");
 const cors = require("cors");
-
+const newsRoutes = require("./routes/newsRoutes");
+const apicache = require("apicache");
+const { handleError } = require("./middlewares");
 const app = express();
-
-// Apply CORS middleware
-app.use(cors());
 let cache = apicache.middleware;
 
-const NEWS_API_URL = "https://newsapi.org/v2/top-headlines";
-const API_KEY = "fe9deb34dd4742f3b67fd26adf9decdb";
+// All Middlwares
+app.use(cors());
+app.use(cache("10 minutes"));
+app.use(handleError);
 
-// Endpoint for news which caches the data for 10 minutes
-app.get("/api/news", cache("10 minutes"), async (req, res) => {
-  try {
-    // Fetch news from the News API
-    const response = axios
-      .get(NEWS_API_URL, {
-        params: {
-          apiKey: API_KEY,
-          country: "us",
-        },
-      })
-      .then((data) => {
-        res.json(data.data.articles.filter((article) => article.source.id));
-      });
-  } catch (error) {
-    return res.status(500).json({ message: "Error fetching news", error });
-  }
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Some unexpected error occured!");
-});
+// All Routes
+app.use("/api/news", newsRoutes);
 
 const PORT = process.env.PORT || 3000;
 
